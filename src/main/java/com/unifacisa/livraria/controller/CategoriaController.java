e7b22761e0e6150577c4e5c9f582c8480da85f2c
didomiranda <109598623+didomiranda@users.noreply.github.com>
2024-11-27 14:41:30 -0300

    Controller atualizado CategoriaController.java

--- src/main/java/com/unifacisa/livraria/controller/CategoriaController.java
+++ src/main/java/com/unifacisa/livraria/controller/CategoriaController.java
@@ -1,5 +1,25 @@
 package com.unifacisa.livraria.controller;
 
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+  @Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+        return categoriaService.listar();
+	}
+	
+	@PostMapping
+	public Categoria salvar(@RequestBody Categoria categoria) {
+        return categoriaService.salvar(categoria);
+  }
+
+  @DeleteMapping("/{id}")    
+  public void deletar(@PathVariable Long id) {
+        categoriaService.deletar(id);
+    }
 
 }
