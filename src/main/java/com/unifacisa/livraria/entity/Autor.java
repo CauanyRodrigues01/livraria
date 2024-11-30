1df345987a0e676800cf7c7b9cd9fa32273ff31c
brunaxmello <bruna.mello@maisunifacisa.com.br>
2024-11-30 18:23:34 -0300

    Feito incompleto relacionamento entre Autor/Categoria

--- src/main/java/com/unifacisa/livraria/entity/Autor.java
+++ src/main/java/com/unifacisa/livraria/entity/Autor.java
@@ -1,9 +1,15 @@
 package com.unifacisa.livraria.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Autor {
@@ -14,6 +20,13 @@ public class Autor {
 	
 	private String nome;
 
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "autor_categoria",
+	joinColumns = @JoinColumn(name = "autor_id"),
+	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private Set<Categoria> categorias;
+	
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/unifacisa/livraria/entity/Categoria.java
+++ src/main/java/com/unifacisa/livraria/entity/Categoria.java
@@ -1,9 +1,13 @@
 package com.unifacisa.livraria.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Categoria {
@@ -13,6 +17,10 @@ public class Categoria {
 	private Long id;
 
 	private String nome;
+	
+	
+	@ManyToMany(mappedBy = "categorias", cascade = CascadeType.ALL)
+	private Set<Autor> autores;
 
 	public Long getId() {
 		return id;
