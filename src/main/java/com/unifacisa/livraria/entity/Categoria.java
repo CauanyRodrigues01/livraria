aad376d6d4ba79fa08c26bce064f852e68a4b7a2
Jeffersonthiagofn <jeffersonthiagofn@gmail.com>
2024-12-02 21:19:44 -0300

    Resolvendo conflitos no merge de develop e livroCategoria

--- src/main/java/com/unifacisa/livraria/entity/Categoria.java
+++ src/main/java/com/unifacisa/livraria/entity/Categoria.java
@@ -2,13 +2,21 @@ package com.unifacisa.livraria.entity;
 
 import java.util.List;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import jakarta.persistence.ManyToMany;
+
+import jakarta.persistence.OneToMany;
+
 
 @Entity
 public class Categoria {
@@ -19,10 +27,16 @@ public class Categoria {
 
 	private String nome;
 	
+
 	
 	@ManyToMany(mappedBy = "categorias")
 	@JsonIgnore  // Ignora a serialização da lista de autores
 	private List<Autor> autores;
+
+	@OneToMany(mappedBy = "categoria")
+	@JsonBackReference
+	private List<Livro> livros;
+
 
 	public Long getId() {
 		return id;
