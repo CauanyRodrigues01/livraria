6e1104d287f5aa94afc57bb1490a621484495197
Jeffersonthiagofn <jeffersonthiagofn@gmail.com>
2024-12-02 21:19:44 -0300

    Realizado o relacionamento entre Categoria e Livro

--- src/main/java/com/unifacisa/livraria/entity/Categoria.java
+++ src/main/java/com/unifacisa/livraria/entity/Categoria.java
@@ -1,9 +1,14 @@
 package com.unifacisa.livraria.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Categoria {
@@ -13,6 +18,10 @@ public class Categoria {
 	private Long id;
 
 	private String nome;
+	
+	@OneToMany(mappedBy = "categoria")
+	@JsonBackReference
+	private List<Livro> livros;
 
 	public Long getId() {
 		return id;
--- src/main/java/com/unifacisa/livraria/entity/Livro.java
+++ src/main/java/com/unifacisa/livraria/entity/Livro.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Livro {
@@ -12,8 +14,13 @@ public class Livro {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String titulo;
+    
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 
-    public Long getId() {
+
+	public Long getId() {
         return id;
     }
 
@@ -28,4 +35,12 @@ public class Livro {
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+    
+    public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 }
