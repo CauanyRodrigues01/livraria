543fa392586a3b4d6e1f3213997e899f4de7763b
didomiranda <109598623+didomiranda@users.noreply.github.com>
2024-12-05 09:18:02 -0300

    Update Descrição.java

--- src/main/java/com/unifacisa/livraria/entity/Descrição.java
+++ src/main/java/com/unifacisa/livraria/entity/Descrição.java
@@ -1,1 +1,40 @@
+package com.unifacisa.livraria.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+public class Descricao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nome;
+
+    @OneToOne(mappedBy = "descricao")
+    private Livro livro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+}
