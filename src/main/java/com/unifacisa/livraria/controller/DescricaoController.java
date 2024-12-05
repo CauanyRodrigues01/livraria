1e75f88c51d462dba5f7b63dcc397e66a0d92a5f
didomiranda <109598623+didomiranda@users.noreply.github.com>
2024-12-05 09:23:45 -0300

    Update DescricaoController.java

--- src/main/java/com/unifacisa/livraria/controller/DescricaoController.java
+++ src/main/java/com/unifacisa/livraria/controller/DescricaoController.java
@@ -1,1 +1,26 @@
+package com.unifacisa.livraria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.unifacisa.livraria.entity.Descricao;
+import com.unifacisa.livraria.service.DescricaoService;
+
+@RestController
+@RequestMapping("/descricoes")
+public class DescricaoController {
+
+    @Autowired
+    private DescricaoService descricaoService;
+
+    @PostMapping
+    public Descricao salvar(@RequestBody Descricao descricao) {
+        return descricaoService.salvar(descricao);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Descricao> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.of(descricaoService.buscarPorId(id));
+    }
+}
