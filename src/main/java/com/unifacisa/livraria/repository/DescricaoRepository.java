4b695bc736db2dec8659751ecaa090d0374fa15e
didomiranda <109598623+didomiranda@users.noreply.github.com>
2024-12-05 09:22:20 -0300

    Update DescricaoRepository.java

--- src/main/java/com/unifacisa/livraria/repository/DescricaoRepository.java
+++ src/main/java/com/unifacisa/livraria/repository/DescricaoRepository.java
@@ -1,1 +1,9 @@
+package com.unifacisa.livraria.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.unifacisa.livraria.entity.Descricao;
+
+@Repository
+public interface DescricaoRepository extends JpaRepository<Descricao, Long> {
+}
