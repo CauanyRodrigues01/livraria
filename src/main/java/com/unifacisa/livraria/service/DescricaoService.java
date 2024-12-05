4841b11ef44bd420fa505290e99673b578163f72
didomiranda <109598623+didomiranda@users.noreply.github.com>
2024-12-05 09:26:20 -0300

    Update DescricaoService.java
    opcional de buscar pelo ID da descricao e nao apenas pelo livro

--- src/main/java/com/unifacisa/livraria/service/DescricaoService.java
+++ src/main/java/com/unifacisa/livraria/service/DescricaoService.java
@@ -1,1 +1,24 @@
+Package com.unifacisa.livraria.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.unifacisa.livraria.entity.Descricao;
+import com.unifacisa.livraria.repository.DescricaoRepository;
+
+@Service
+public class DescricaoService {
+
+    @Autowired
+    private DescricaoRepository descricaoRepository;
+
+    public Optional<Descricao> buscarPorId(Long id) {
+        return descricaoRepository.findById(id);
+    }
+
+    public Descricao salvar(Descricao descricao) {
+        return descricaoRepository.save(descricao);
+    }
+}
